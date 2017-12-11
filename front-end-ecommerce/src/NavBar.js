b8141ee83180b1a035544dc10562e42b52cb97af
Jason Li <Yunf.li37@gmail.com>
2017-12-11 12:28:15 -0500

    add navbar

---
+++ front-end-ecommerce/src/NavBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component{
+  constructor(){
+    super();
+  }
+
+
+  render(){
+    return(
+      <h1>Navbar Sanity Check</h1>
+    )
+  }
+}
+
+export default Navbar;
