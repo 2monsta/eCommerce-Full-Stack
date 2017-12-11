d5f8e1407841922c9304856bbfe34fb813f94f84
Jason Li <Yunf.li37@gmail.com>
2017-12-11 14:21:10 -0500

    add slick images/modules

---
+++ front-end-ecommerce/src/components/SlickSlider.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SlickSlider extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Slider would be here</h1>
+      </div>
+    )
+  }
+}
+
+export default SlickSlider;
