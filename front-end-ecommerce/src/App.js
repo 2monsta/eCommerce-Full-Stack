7f56874dc539af927aa68bf0f267e58488c78e64
Jason Li <Yunf.li37@gmail.com>
2017-12-12 13:42:22 -0500

    add register page

--- front-end-ecommerce/src/App.js
+++ front-end-ecommerce/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+//CUSTOM COMPOENT
 import NavBar from './components/NavBar';
 import SlickSlider from './components/SlickSlider';
+import Register from './containers/Register';
+import Home from './components/Home';
+
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +16,10 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <Route exact path='/' component={SlickSlider}/>
+          <div className={"container"}>
+            <Route exact path={'/'} component={Home}/>
+            <Route exact path={"/register"} component={Register}/>
+          </div>
         </div>
       </Router>
     );
--- front-end-ecommerce/src/components/NavBar.js
+++ front-end-ecommerce/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Navbar extends Component{
   constructor(){
@@ -15,18 +16,18 @@ class Navbar extends Component{
                 <div className={"col s7"}>
                   <ul className="hide-on-med-and-down">
                     <div className={"row"}>
-                      <li className={"col s2"}>Home</li>
-                      <li className={"col s2"}>Shop</li>
-                      <li className={"col s2"}>About Us</li>
-                      <li className={"col s3"}>Contact Us</li>
+                      <li className={"col s2"}><Link to={"/"}>Home</Link></li>
+                      <li className={"col s2"}><Link to={"/shop"}>Shop</Link></li>
+                      <li className={"col s2"}><Link to={"/about"}>About Us</Link></li>
+                      <li className={"col s3"}><Link to={"/contact"}>Contact Us</Link></li>
                     </div>
                   </ul>
                 </div>
                 <ul className="side-nav" id="mobile-demo">
-                  <li>Home</li>
-                  <li>Shop</li>
-                  <li>About Us</li>
-                  <li>Contact Us</li>
+                  <li><Link to={"/"}>Home</Link></li>
+                  <li><Link to={"/shop"}>Shop</Link></li>
+                  <li><Link to={"/about"}>About Us</Link></li>
+                  <li><Link to={"/contact"}>Contact Us</Link></li>
                 </ul>
                 <div className={"col s3 offset-s2"}>
                   <form>
@@ -45,7 +46,7 @@ class Navbar extends Component{
                   <p>classic logo goes here</p>
                 </div>
                 <div className={"offset-s4 col s5 hide-on-med-and-down right-align"}>
-                  <p>Sign in or Create an account | (0) items in cart | ($0.00)</p>
+                  <p className={"linkToLogin"}><Link to={"/login"}>Sign in</Link> or <Link to={"/register"}>Create an account</Link> | (0) items in cart | ($0.00)</p>
                 </div>
               </div>
             </div>
