428cd25307cad2b9e5ead18da819b033d9e4fad8
Jason Li <Yunf.li37@gmail.com>
2017-12-17 14:42:07 -0500

    update read me

--- front-end-ecommerce/src/containers/UserProfile.js
+++ front-end-ecommerce/src/containers/UserProfile.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {Input, Button} from 'react-materialize';
 
 class UserProfile extends Component{
   render(){
     return(
-      <h1>User Profile</h1>
+      <div>
+        <h1>User Profile</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input s={12} id={"name"} label="New Full Name" />
+          <Input type="email" id={"email"} label="Email" s={12} />
+          <Input type="password" id={"password"} label="password" s={12} />
+          <Input s={12} id={"accountType"} label="Account Type" type={'text'}  />
+          <Input s={12} id={"city"} label="City" />
+          <Input s={12} id={"state"} label="State" />
+          <Input s={12} id={"salesRep"} label="Sales Rep" />
+          <Button waves={"light"}>Register </Button>
+        </form>
+      </div>
     )
   }
 }
